952cac0401a8b7035317539634d4d654d1302593
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 23:24:27 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true, message: 'can not be empty'
+  validates :email, presence: true, uniqueness: true
 end
