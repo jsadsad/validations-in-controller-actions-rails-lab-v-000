9b7ecc52cfdb8da46f4502068bb1d94dedcfa5ad
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-08 23:39:42 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       @post.save
       redirect_to post_path(@post)
     else
